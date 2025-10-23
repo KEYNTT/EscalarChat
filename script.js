fafbb6bedcc52ecb9cd0543aadf1ce88dadf305a
KEYNTT <87054104+KEYNTT@users.noreply.github.com>
2025-10-23 13:35:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import TubesCursor from "https://cdn.jsdelivr.net/npm/threejs-components@0.0.19/build/cursors/tubes1.min.js"
+import TubesCursor from "https://cdn.jsdelivr.net/npm/threejs-components@0.0.19/build/cursors/tubes1.min.js";
 
 const app = TubesCursor(document.getElementById('canvas'), {
   tubes: {
@@ -8,18 +8,18 @@ const app = TubesCursor(document.getElementById('canvas'), {
       colors: ["#83f36e", "#fe8a2e", "#ff008a", "#60aed5"]
     }
   }
-})
+});
 
 document.body.addEventListener('click', () => {
-  const colors = randomColors(3)
-  const lightsColors = randomColors(4)
-  console.log(colors, lightsColors)
-  app.tubes.setColors(colors)
-  app.tubes.setLightsColors(lightsColors)
-})
+  const colors = randomColors(3);
+  const lightsColors = randomColors(4);
+  console.log(colors, lightsColors);
+  app.tubes.setColors(colors);
+  app.tubes.setLightsColors(lightsColors);
+});
 
-function randomColors (count) {
-    return new Array(count)
-        .fill(0)
-        .map(() => "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0'))
+function randomColors(count) {
+  return new Array(count)
+    .fill(0)
+    .map(() => "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0"));
 }
